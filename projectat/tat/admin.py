891477fe6e7be0bc3311019532c0a4023a29c6d8
Yuvraj128 <107023082+Yuvraj128@users.noreply.github.com>
2022-12-07 19:33:19 +0530

    Delete projectat directory

--- projectat/tat/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin               # Geekyshows:- 1:21:00
-
-from .models import Destination
-# Register your models here.
-@admin.register(Destination)
-
-
-class DestinationModelAdmin(admin.ModelAdmin):
-    list_display = ['id','title','prod_img']
